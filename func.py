8350594bc252f40d0f8c1cba018c03efaa3eabb6
Bianca <biancaft51@gmail.com>
2024-11-13 16:14:35 -0300

    ultimas funcionalidades do sistema

--- func.py
+++ func.py
@@ -1,14 +1,17 @@
 from flor import Flor
+from cliente import Cliente
 
 class Sistema: 
 
     def __init__(self):
         self.estoque = []
+        self.clientes = []
 
+#----------------------------- estoque -----------------------------------
     def adiciona_flor(self, especie, quantidade, preco):
         flor = Flor(especie, quantidade, preco)
         self.estoque.append(flor)
-        print(f"Flor: {flor.especie} | Quantidade: {flor.quantidade} | Preco: {flor.preco} adicionada ao estoque com sucesso!!!")
+        print(f"Flor: {flor.especie} adicionada ao estoque com sucesso!!!")
 
     def imprime_estoque(self):
         print("***** ESTOQUE *****")
@@ -18,6 +21,7 @@ class Sistema:
             print(f" Especie: {flores.especie} | Quantidade: {flores.quantidade} | Preco: {flores.preco} ")
             print("---------------------------------------------------\n")
 
+#----------------------------- financeiro -----------------------------------
     def compra_por_flor(self):
         especie = input("Qual especie deseja calcular o valor da compra? ")
         for flor in self.estoque:
@@ -47,8 +51,32 @@ class Sistema:
             else:
                 print("Tamanho ou especie não identificada!!!")
 
-    def cadastro_cliente(self):
-        print("nada ainda cadastro cliente")
+#----------------------------- cliente -----------------------------------
+    def cadastro_cliente(self, nome, telefone, idade, email):
+        cliente = Cliente(nome, telefone, idade, email)
+        if not any(c.nome == cliente.nome and c.email == cliente.email for c in self.clientes): #verifica se ja existe algum cliente de mesmo nome e email na lista
+            self.clientes.append(cliente)
+            print(f"Cliente {cliente.nome} adicionado com sucesso!")
+        else:
+            print("Cliente com o mesmo nome e email já existe na lista.")
 
-    def pedidos(self):
-        print("nada ainda pedidos")
+    def clientes_cadastrados(self):
+        print("***** LISTA DE CLIENTES *****")
+        for cliente in self.clientes:
+            if not self.clientes:
+                print("Não existe nenhum cliente cadastrado")
+            print(f"Nome: {cliente.nome} | Telefone: {cliente.telefone} | Idade: {cliente.idade} | E-mail: {cliente.email}")
+
+    def exclui_cliente(self):
+        excluirN = input("Informe o email do cliente que deseja excluir: ")
+        for cliente in self.clientes:
+            if excluirN == cliente.email:
+                self.clientes.remove(cliente)
+                print("Cliente excluido com sucesso!!!")
+                break
+        else:
+            print("Cliente não encontrado")
+
+
+
+
--- main.py
+++ main.py
@@ -4,11 +4,16 @@ def mostra_menu():
     sistema = Sistema() 
     while True:
         print("\n***********MENU***********")
+        print("***********ESTOQUE***********")
         print("ADICIONAR NOVAS ESPECIES AO ESTOQUE [1]")
         print("ESPECIES DISPONÍVEIS NO ESTOQUE [2]")
+        print("***********FINANCEIRO***********")
         print("VALOR TOTAL DA COMPRA DE UMA ESPECIE [3]")
         print("VALOR DO BUQUE [4]")
-        print("SAIR [5]")
+        print("***********CLIENTES***********")
+        print("CADASTRAR UM NOVO CLIENTE [5]")
+        print("CLIENTES CADASTRADOS [6]")
+        print("EXCLUIR CADASTRO DE CLIENTE [7]")
         opcao = input("Informe o número correspondente a opcao que deseja:")
         if opcao == '1':
             sistema.adiciona_flor("Rosa", 25, 5)
@@ -30,6 +35,21 @@ def mostra_menu():
         elif opcao == '4':
             sistema.venda_buque() 
         elif opcao == '5':
+            sistema.cadastro_cliente ("João", "123456789", 30, "joao@example.com")
+            sistema.cadastro_cliente ("Maria", "987654321", 25, "maria@example.com")
+            sistema.cadastro_cliente ("Carlos", "456123789", 35, "carlos@example.com")
+            sistema.cadastro_cliente ("Ana", "789456123", 28, "ana@example.com")
+            sistema.cadastro_cliente ("Lucas", "321654987", 40, "lucas@example.com")
+            sistema.cadastro_cliente ("Fernanda", "654789321", 22, "fernanda@example.com")
+            sistema.cadastro_cliente ("Ricardo", "987123456", 33, "ricardo@example.com")
+            sistema.cadastro_cliente ("Camila", "123789456", 27, "camila@example.com")
+            sistema.cadastro_cliente ("Rafael", "456987123", 29, "rafael@example.com")
+            sistema.cadastro_cliente ("Juliana", "789321654", 31, "juliana@example.com")
+        elif opcao == '6':
+            sistema.clientes_cadastrados() 
+        elif opcao == '7':
+            sistema.exclui_cliente() #NÂO EXISTE AINDA !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        elif opcao == '7':
             print("Saindo do sistema...")
             break
         else:
